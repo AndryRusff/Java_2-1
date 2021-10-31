eb0d818a9085cf1fac19cf674080aa88c2d65e6b
AndryRusff <jam.track@yandex.ru>
2021-11-01 00:02:53 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 999;
+        int oneBonusMile = 20;
+        int totalBonusMiles = ticketPrice / oneBonusMile;
+        System.out.println(totalBonusMiles);
+    }
+}
